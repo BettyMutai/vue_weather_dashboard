1fbeeaa99de8c42d12888672909f8c63f768a23d
Betty Mutai <bettymutai00@gmail.com>
2022-03-30 16:34:22 +0300

    Create component(placeholder) and child components

---
+++ src/components/Highlights.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Highlights.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TempVarChart.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "TempVarChart.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
